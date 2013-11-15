3ddebc92da3abcaf0de3972463ce964182a185db
Amit Apple <amitap@microsoft.com>
2013-11-15 11:25:04 -0800

    initial

---
+++ ContinuousHelloWorld/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading;
+
+namespace ContinuousHelloWorld
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Hello World!!! " + DateTime.UtcNow);
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
